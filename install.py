cdc188176799964ecdad99d686dbab92609f301d
toballz <42551340+toballz@users.noreply.github.com>
2019-08-01 19:51:21 -0400

    Update install.py

--- install.py
+++ install.py
@@ -2,14 +2,14 @@ import os
 
 d = os.path.dirname(os.path.abspath(__file__))
 
-os.system("chmod +x bru")
+os.system("chmod +x "+d+"/bru")
 if os.path.exists("/usr/local/bin/brute"):
  print("Updating Folder.....")
 else:
   os.system("sudo mkdir /usr/local/bin/brute")
 
-os.system("sudo mv bru /usr/local/bin")
-os.system("sudo mv psw.txt /usr/local/bin/brute")
+os.system("sudo mv "+d+"/bru /usr/local/bin")
+os.system("sudo mv "+d+"/psw.txt /usr/local/bin/brute")
 os.system("sudo ln -s /usr/local/bin/brute/psw.txt "+d)
 os.system("sudo ln -s /usr/local/bin/brute/bru "+d)
-print "Installation Successfull.\nYou can access the file by typing bru"
+print "BruteForce Installation Successfull.\nYou can access the file by typing bru from any directory"
