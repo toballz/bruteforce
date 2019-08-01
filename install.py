187261f93ba5281513ce74e868eb3d503ac00721
toballz <42551340+toballz@users.noreply.github.com>
2019-08-01 09:02:25 -0400

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,14 @@
 import os
 
 d = os.path.dirname(os.path.abspath(__file__))
+p = Path("/usr/local/bin/brute")
+
 os.system("chmod +x bru")
-os.system("sudo mkdir /usr/local/bin/brute")
+if p.is_file():
+ print("Writing.....")
+else:
+  os.system("sudo mkdir /usr/local/bin/brute")
+
 os.system("sudo mv bru /usr/local/bin")
 os.system("sudo mv psw.txt /usr/local/bin/brute")
 os.system("sudo ln -s /usr/local/bin/brute/psw.txt "+d)
