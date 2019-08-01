25685210b0a5b58c9e404acf64902e37d84280ee
toballz <42551340+toballz@users.noreply.github.com>
2019-08-01 07:35:02 -0400

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,6 +1,7 @@
-import requests
+#!/usr/bin/env python
+import requests,os
 
-#change post or get if get or post
+os.system("clear")
 #change username:name password:name and other param:name
 class bc:
     OKGREEN = '\033[92m'
@@ -10,7 +11,7 @@ class bc:
 #cookies
 #s=requests.Session();c=s.get("http://10.0.2.2");ck=dict(c.cookies);cc = ck['_usse']
 
-url = raw_input("\nUrl post: ")
+url = raw_input("Url post: ")
 user = (raw_input("Enter ((formname:user)): ").strip()).split(":")
 ter = raw_input("Invalid login response: ")
 pswfile = raw_input("Enter psw file [empty = psw.txt]: ").strip("' ") or "psw.txt";pswlines = open(pswfile, "r").readlines()
